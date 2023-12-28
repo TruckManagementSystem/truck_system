ee1293eb91e6a54851d6cd86edeff1a1b77ca976
Yahya saad <yahyasaad.me@gmail.com>
2023-12-28 19:23:26 +0530

    nav corrected & profile created

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Account</title>
+</head>
+<body>
+    user name
+    user email
+    user password
+    street address district
+    City/Town
+    Zip/Potalcode
+    account created at 
+    user id
+    update - btn
+</body>
+</html>
